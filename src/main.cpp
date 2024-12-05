dcee8d6335a1a7fa7ca5bdb17788ee11d96c3819
Jesse Born <jesse.born@gmx.ch>
2024-12-05 10:44:40 +0100

    a*, time cost and params

--- src/main.cpp
+++ src/main.cpp
@@ -7,38 +7,11 @@
 
 using namespace std;
 
-template <typename T>
-struct Tuple
-{
-  T a;
-  T b;
-
-  Tuple(T f, T s) : a(f), b(s) {}
-};
-
-// static Tuple<double> GetWorldCoordinates(int x, int y, double *fwdTransform)
-// {
-
-//   double worldX = fwdTransform[0] + x * fwdTransform[1];
-//   double worldY = fwdTransform[3] + y * fwdTransform[5];
-
-//   return Tuple<double>(worldX, worldY);
-// }
-
-// static Tuple<int> GetPixelCoordinates(double x, double y, double *fwdTransform)
-// {
-//   int pixelY = (y - fwdTransform[0]) / fwdTransform[1];
-//   int pixelX = (x - fwdTransform[3]) / fwdTransform[5];
-
-//   return Tuple<int>(pixelX, pixelY);
-// }
-
 static void Exit(int code)
 {
-  cout << "Exiting..." << endl;
+  cout << "Exit..." << endl;
 
   GDALDestroy();
-
   exit(code);
 }
 
@@ -65,7 +38,19 @@ struct cmp_walkcost
   }
 };
 
-void runWalkOnRasters(GeoTiffLoader &riskmap, GeoTiffLoader &dem, int nXSize, int nYSize)
+float walk_time_cost(float demValueA, float demValueB, float distance)
+{
+  const float cd = 0.25;
+  const float ce = 2.5;
+
+  float gradient = (demValueB - demValueA) / distance;
+  float elevation_diff = demValueB - demValueA;
+  float steepness_cost = abs(gradient) >= tan(45.0 / 180.0 * M_PI) ? 5.0 : 1.0;
+
+  return distance * cd + abs(elevation_diff) * ce * steepness_cost * distance;
+}
+
+std::vector<WalkItem> runWalkOnRasters(GeoTiffLoader &riskmap, GeoTiffLoader &dem, int nXSize, int nYSize)
 {
   float startLat = 2693252.0;
   float startLon = 1203811.0;
@@ -76,7 +61,6 @@ void runWalkOnRasters(GeoTiffLoader &riskmap, GeoTiffLoader &dem, int nXSize, in
   WalkItem end = {0, 0};
   riskmap.convertLatLonToPixel(endLat, endLon, end.x, end.y);
 
-  // TODO: Implement A* algorithm
   // TODO: Check if start and end are within the bounds of the raster
   // TODO: Check if start and end are not NODATA_VALUE
 
@@ -111,8 +95,14 @@ void runWalkOnRasters(GeoTiffLoader &riskmap, GeoTiffLoader &dem, int nXSize, in
 
     if (current.x_riskmap == end.x && current.y_riskmap == end.y)
     {
-      cout << "Path found" << endl;
-      break;
+      auto path = std::vector<WalkItem>();
+      WalkItem current = {end.x, end.y};
+      while (current.x != start.x_riskmap || current.y != start.y_riskmap)
+      {
+        path.push_back(current);
+        current = cameFrom[current.x + current.y * nXSize];
+      }
+      return path;
     }
 
     for (int i = -1; i <= 1; i++)
@@ -139,9 +129,10 @@ void runWalkOnRasters(GeoTiffLoader &riskmap, GeoTiffLoader &dem, int nXSize, in
           continue;
         }
 
-        next.totalCost += *nextRiskmapValue;
+        float heuristic = sqrt((end.x - next.x_riskmap) * (end.x - next.x_riskmap) + (end.y - next.y_riskmap) * (end.y - next.y_riskmap));
+        next.totalCost += 50.0 * (*nextRiskmapValue) + walk_time_cost(*demValue, *nextDemValue, sqrt(i * i + j * j) * 10.0) + heuristic * 0.1;
 
-        // cameFrom.at(next.x_riskmap + next.y_riskmap * riskmap.GetNXSize()) = {current.x_riskmap, current.y_riskmap};
+        cameFrom[next.x_riskmap + next.y_riskmap * riskmap.GetNXSize()] = {current.x_riskmap, current.y_riskmap};
         q.push(next);
       }
     }
@@ -173,7 +164,10 @@ int main(int argc, char *argv[])
 
   cout << "Value at 2662390.000000, 1239740.000000: " << a << "@ " << b << "müM" << endl;
 
-  runWalkOnRasters(*riskmap_loader, *dem_loader, riskmap_loader->GetNXSize(), riskmap_loader->GetNYSize());
+  for (int i = 0; i < 10; i++)
+  {
+    runWalkOnRasters(*riskmap_loader, *dem_loader, riskmap_loader->GetNXSize(), riskmap_loader->GetNYSize());
+  }
 
   riskmap_loader->~GeoTiffLoader();
   dem_loader->~GeoTiffLoader();
